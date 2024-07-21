2c19a82b920718d2fb38e09057c095d1dabe34bf
Taiki-Ishigaki <ishigaki@ynl.t.u-tokyo.ac.jp>
2024-07-21 20:54:11 +0900

    💡IMPROVE:  lins struct

--- kotsu/link_struct.py
+++ kotsu/link_struct.py
@@ -50,10 +50,8 @@ class LinkStruct(LinkStruct_):
   def set_dof(self):
     self.joint_dof = self._joint_dof(self.joint_type)
     self.link_dof = self._link_dof(self.link_type)
+    self.dof = self.joint_dof + self.link_dof
     self.dof_index = self.joint_dof + self.link_dof
-    
-  def dof(self):
-    return self.joint_dof(self) + self.link_dof(self)
   
   def set_connent_frame(self):
     self.connent_frame = SE3(self.connect_rot, self.connect_pos).matrix()
@@ -62,31 +60,3 @@ class LinkStruct(LinkStruct_):
   def set_connent_adj_frame(self):
     self.connent_adj_frame =  SE3(self.connect_rot, self.connect_pos).adjoint()
     return self.connent_adj_frame
-    
-def read_model_file(xml_data):
-  robot = ET.fromstring(xml_data)
-
-  links = []
-  dof_index = 0
-  
-  for i in range(len(robot)):
-    links.append(LinkStruct())
-
-    links[i].name = robot[i].attrib.get('name')
-    links[i].joint_type = robot[i].attrib.get('joint_type')
-    links[i].link_type = robot[i].attrib.get('link_type')
-    links[i].connection = robot[i].attrib.get('connection')
-    links[i].connect_pos = np.array(eval(robot[i].attrib.get('connect_pos')))
-    links[i].connect_rot = np.array(eval(robot[i].attrib.get('connect_rot')))
-    links[i].cog = np.array(eval(robot[i].attrib.get('cog')))
-    links[i].mass = eval(robot[i].attrib.get('mass'))
-    links[i].inertia_param = np.array(eval(robot[i].attrib.get('inertia_param')))
-
-    links[i].set_dof()
-    links[i].set_connent_frame()
-    links[i].set_connent_adj_frame()
-
-    links[i].dof_index = dof_index
-    dof_index += links[i].dof
-
-  return links
