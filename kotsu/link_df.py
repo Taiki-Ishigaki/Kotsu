79e5512895748cc96f3f43d0850cdc2d21a417c8
Taiki-Ishigaki <ishigaki@ynl.t.u-tokyo.ac.jp>
2024-07-21 11:17:52 +0900

    💡IMPROVE:

--- kotsu/link_df.py
+++ kotsu/link_df.py
@@ -11,16 +11,14 @@ from mathrobo.se3 import *
 
 class LinkDF:
   def __init__(self, links, aliases_, separator_ = "_"):
-    self.links = links
-
     self.aliases = aliases_
     self.separator = separator_
 
     self.df = pl.DataFrame()
     self.set_link_df(links)
 
-  def set_link_df(self):
-    for l in self.links:
+  def set_link_df(self, links):
+    for l in links:
       for a in self.aliases:
         alias_name = l.name + self.separator + a
         self.df = self.df.with_columns([pl.Series(name=alias_name, dtype=pl.List(pl.Float64))])
@@ -36,16 +34,15 @@ class LinkGenDF(LinkDF):
     self.aliases = ["coord", "veloc", "accel", "force"]
 
     self.df = pl.DataFrame()
-    self.set_link_df()
+    self.set_link_df(links)
 
 class LinkStateDF(LinkDF):
   def __init__(self, links, separator_ = "_"):
-    self.links = links
     self.separator = separator_
     self.aliases = ["pos", "rot", "vel", "acc"]
 
     self.df = pl.DataFrame()
-    self.set_link_df()
+    self.set_link_df(links)
 
   def link_frame(self, id):
     _pos = self.df[self.links[id].name+"_pos"].to_numpy()
