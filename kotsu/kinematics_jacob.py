b239005c24b7f7888c305d9723546a2c91b99413
Taiki Ishigaki <ishigaki@ynl.t.u-tokyo.ac.jp>
2024-08-14 05:39:37 +0000

    👌 IMPROVE:

--- kotsu/kinematics_jacob.py
+++ kotsu/kinematics_jacob.py
@@ -4,36 +4,6 @@
 
 import numpy as np
 
-def search_neiborhood(robot, link_id):
-  link_list = []
-  link = robot.links[link_id]
-  for j_id in link.connect_joint:
-    joint = robot.joints[j_id]
-    link_list.append(joint.connect_link)
-
-  return link_list
-
-'''
-閉リンク構造未対応
-'''
-def search_root_route(robot, link_id, tree_route):
-  link = robot.links[link_id]
-
-  if(link.is_edge() and (not link.is_root())):
-    return False
-  else:
-    link_id_list = search_neiborhood(robot, link_id)
-    for l_id in link_id_list:
-      if (robot.links[l_id].is_root()):
-        route.append(l_id)
-      else:
-        route = search_root_route(robot, l_id, tree_route)
-        if (route):
-          route.append(link_id)
-    return route
-
-
-
 class BasicJacobian:
 
   @staticmethod
