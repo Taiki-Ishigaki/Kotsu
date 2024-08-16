23f942ee32aa4a12e2596b58f2552efc6af40194
Taiki-Ishigaki <ishigaki@ynl.t.u-tokyo.ac.jp>
2024-08-17 03:04:44 +0900

    💡IMPROVE: df

--- kotsu/df/robot_df.py
+++ kotsu/df/robot_df.py
@@ -27,34 +27,3 @@ class RobotDF:
       for a in self.aliases:
         alias_name = name + self.separator + a
         self.df = self.df.with_columns([pl.Series(name=alias_name, dtype=pl.List(pl.Float64))])
-
-class RobotGenDF(RobotDF):
-  def __init__(self, robot_, aliases_ = ["coord", "veloc", "accel", "force"], separator_ = "_"):
-    self.separator = separator_
-    self.aliases = aliases_
-
-    self.df = pl.DataFrame()
-    self.set_gen_df(robot_)
-    
-  def set_gen_df(self, robot_):
-    for a in self.aliases:
-      for j in robot_.joints:
-        alias_name = j.name + self.separator + a
-        self.df = self.df.with_columns([pl.Series(name=alias_name, dtype=pl.List(pl.Float64))])
-      for l in robot_.links:
-        alias_name = l.name + self.separator + a
-        self.df = self.df.with_columns([pl.Series(name=alias_name, dtype=pl.List(pl.Float64))])
-
-class RobotStateDF(RobotDF):
-  def __init__(self, robot_, aliases_ = ["pos", "rot", "vel", "acc"], separator_ = "_"):
-    self.separator = separator_
-    self.aliases = aliases_
-
-    self.df = pl.DataFrame()
-    self.set_link_state_df(robot_)
-    
-  def set_link_state_df(self, robot_):
-    for l in robot_.links:
-        for a in self.aliases:
-          alias_name = l.name + self.separator + a
-          self.df = self.df.with_columns([pl.Series(name=alias_name, dtype=pl.List(pl.Float64))])
