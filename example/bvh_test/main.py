f99398498864f33f61c494fd0154cfb6dbf8faf6
Taiki-Ishigaki <ishigaki@ynl.t.u-tokyo.ac.jp>
2024-08-17 20:56:40 +0900

    💡IMPROVE: bvh example

--- example/bvh_test/main.py
+++ example/bvh_test/main.py
@@ -2,10 +2,17 @@ import numpy as np
 import kotsu
 
 def main():
-  with open('./test.bvh', 'r') as f:
+  with open('./motion.bvh', 'r') as f:
+  # with open('./test.bvh', 'r') as f:
     robot = kotsu.bvh.BvhRobot.init_from_bvh_file(f.read())
-    
-  print(robot.motions.df.df)
+
+  print(robot.state.df.df)
+
+  robot.update_kinematics(robot.motions.motion_vecs[1])
+  
+  print(robot.state.df.df)
+  
+  kotsu.show_kotsu(robot, robot.state)
 
 if __name__ == "__main__":
     main()
