75bfdc6a99398327d412766b25eccca5def0e6bb
Taiki-Ishigaki <ishigaki@ynl.t.u-tokyo.ac.jp>
2024-10-27 21:27:11 +0900

    💡IMPROVE: kinematics

--- kotsu/kinematics/kinematics.py
+++ kotsu/kinematics/kinematics.py
@@ -13,25 +13,55 @@ from ..state import *
 
 class LinkKinematics:
   @staticmethod
-  def joint_local_frame(theta, b):
-    if len(theta) != 0:
-      v = b@theta
+  def joint_local_frame(joint, joint_angle):
+    if len(joint_angle) != 0:
+      v = joint.joint_select_mat@joint_angle
     else:
-      v = b@np.zeros(1)
-    frame = mr.SE3.adj_mat(v)
+      v = joint.joint_select_mat@np.zeros(1)
+    frame = mr.SE3.exp_adj(v)
     return frame
-
+  
+  @staticmethod
+  def joint_local_vel(joint, joint_vel):
+    if len(joint_vel) != 0:
+      vel = joint.joint_select_mat @ joint_vel
+    else:
+      vel = np.zeros(6)
+    return vel
+  
+  @staticmethod
+  def joint_local_acc(joint, joint_acc):
+    if len(joint_acc) != 0:
+      acc = joint.joint_select_mat @ joint_acc
+    else:
+      acc = np.zeros(6)
+    return acc
+  
   @staticmethod
   def link_rel_frame(link, joint, parent, joint_coord):
     if parent:
       p_connect_frame = parent.connect_adj_frames[joint.id] 
     else:
       p_connect_frame = np.identity(6)
-    joint_frame = LinkKinematics.joint_local_frame(joint_coord, joint.joint_select_mat)
+    joint_frame = LinkKinematics.joint_local_frame(joint, joint_coord)
     connect_frame = link.connect_adj_frames[joint.id]
     rel_frame = p_connect_frame @ joint_frame @ connect_frame
     return rel_frame
 
+  @staticmethod
+  def link_rel_vel(link, joint, joint_vel):
+    local_vel = LinkKinematics.joint_local_vel(joint, joint_vel)
+    connect_frame = link.connect_adj_frames[joint.id]
+    rel_vel = np.linalg.inv(connect_frame) @ local_vel
+    return rel_vel
+  
+  @staticmethod
+  def link_rel_acc(link, joint, joint_acc):
+    local_acc = LinkKinematics.joint_local_acc(joint, joint_acc)
+    connect_frame = link.connect_adj_frames[joint.id]
+    rel_acc = np.linalg.inv(connect_frame) @ local_acc
+    return rel_acc
+  
   @staticmethod
   def kinematics(link, joint, parent, motinos, state):
     joint_coord = motinos.joint_coord(joint)
@@ -44,21 +74,6 @@ class LinkKinematics:
     rel_frame = LinkKinematics.link_rel_frame(link, joint, parent, joint_coord)
     frame = p_link_frame @ rel_frame
     return frame
-  
-  @staticmethod
-  def joint_local_vel(joint, joint_vel):
-    if len(joint_vel) != 0:
-      vel = joint.joint_select_mat @ joint_vel
-    else:
-      vel = np.zeros(6)
-    return vel
-
-  @staticmethod
-  def link_rel_vel(link, joint, joint_vel):
-    local_vel = LinkKinematics.joint_local_vel(joint, joint_vel)
-    connect_frame = link.connect_adj_frames[joint.id]
-    rel_vel = np.linalg.inv(connect_frame) @ local_vel
-    return rel_vel
 
   @staticmethod
   def vel_kinematics(link, joint, parent, motinos, state):
@@ -72,21 +87,6 @@ class LinkKinematics:
     rel_vel = LinkKinematics.link_rel_vel(link, joint, joint_veloc)
     vel = np.linalg.inv(rel_frame) @ link_vel  + rel_vel
     return vel
-  
-  @staticmethod
-  def joint_local_acc(joint, joint_acc):
-    if len(joint_acc) != 0:
-      acc = joint.joint_select_mat @ joint_acc
-    else:
-      acc = np.zeros(6)
-    return acc
-
-  @staticmethod
-  def link_rel_acc(link, joint, joint_acc):
-    local_acc = LinkKinematics.joint_local_acc(joint, joint_acc)
-    connect_frame = link.connect_adj_frames[joint.id]
-    rel_acc = np.linalg.inv(connect_frame) @ local_acc
-    return rel_acc
 
   @staticmethod
   def acc_kinematics(link, joint, parent, motinos, state):
